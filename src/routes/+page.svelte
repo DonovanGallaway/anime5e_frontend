8515f30b6d0344eb3487db57e7f909bd75887562
Donovan Gallaway <donovangallaway@gmail.com>
2024-01-15 00:41:17 -0800

    add basic attribute add/delete

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,6 @@
     import { type Character } from '../interfaces/character'
     
     let total_points: number = 80;
-    let points_text: string = `${total_points}`
     let points_used: number = 0;
     let character: Character = {
         attributes: []
@@ -14,9 +13,18 @@
         character = {...changes}
     }
 
-    const changePoints = (value: number) => {
-        total_points -= value
+    const changePoints = (change: number) => {
+        // can be positive or negative
+        points_used += change
     }
+
+    const removeAttribute = (att: any, index: number) => {
+        const point_change = att.rank_cost * att.current_rank
+        const change = character.attributes?.toSpliced(index, 1)
+        character = {...character, attributes: change}
+        changePoints(0 - point_change)
+    }
+
 </script>
 
 <h1>Anime 5e Character Builder</h1>
@@ -34,9 +42,12 @@
     <h2>Character</h2>
     Attributes:
     {#if character.attributes}
-        "I promise I'm here!"
-        {#each character.attributes as assigned_attribute}
-                <span>{assigned_attribute.attribute_name}</span>
+        {#each character.attributes as assigned_attribute, i}
+           <div>
+                {assigned_attribute.attribute_name} (assigned_attribute.details)
+                Ranks: {assigned_attribute.current_rank} Cost: {assigned_attribute.current_rank * assigned_attribute.rank_cost}
+                <button on:click={()=>{removeAttribute(assigned_attribute, i)}}>X</button>
+           </div>     
         {/each}
     
     {/if}
@@ -46,7 +57,8 @@
 <div>
     <h2>Attributes</h2>
     {#each attribute_list as attribute}
-        <Attribute attribute={attribute} character={character} changeChar={changeChar}/>
+        <Attribute attribute={attribute} character={character} changeChar={changeChar} changePoints={changePoints}/>
+        <br>
     {/each}
     
 </div>
--- src/routes/attribute.svelte
+++ src/routes/attribute.svelte
@@ -5,9 +5,11 @@
     export let attribute: AttributeType
     export let character: Character
     export let changeChar
+    export let changePoints
 
     let details: string = ''
     let detail_show: boolean = false
+    let current_rank: number = 1
 
 </script>
 
@@ -15,14 +17,19 @@
     <span>{attribute.attribute_name}</span>
     <span>Rank Cost: {attribute.rank_cost}</span>
     <button on:click={()=>detail_show = true}>Add Attribute</button>
+    <span>Ranks to Add: {current_rank}</span>
+    <button on:click={()=>current_rank++}>+</button>
+    <button on:click={()=>{if(current_rank >= 0){current_rank--}}}>-</button>
     {#if detail_show}
         <form>
             <label for='attribute'>Details:</label>
             <input type='text' bind:value={details}/>
             <button on:click={()=>{
-                attribute.details = details;
+                attribute.details = details
+                attribute.current_rank = current_rank
                 character.attributes?.push(attribute)
                 changeChar(character)
+                changePoints(current_rank * attribute.rank_cost)
                 detail_show = false
             }}>Add</button>
             <button on:click={()=>detail_show = false}>x</button>
