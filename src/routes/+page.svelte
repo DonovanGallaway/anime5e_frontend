9b7c22dc4ff7b4fc8cac1adab748e12ce4ba2a12
Donovan Gallaway <donovangallaway@gmail.com>
2024-01-14 19:05:29 -0800

    add json data, pushing to GH for collab

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang='ts'>
+    let total_points: number = 80;
+    let points_text: string = `${total_points}`
+    let points_used: number = 0;
+
+    const changePoints = (value: number) => {
+        total_points -= value
+    }
+</script>
+
+<h1>Anime 5e Character Builder</h1>
+
+<div class="point-counter">
+    <div>
+        <h2>Total Points: {total_points}</h2>
+        <button on:click={()=>total_points++}>+</button>
+        <button on:click={()=>total_points--}>-</button>
+    </div>
+    <p>Points Remaining: {total_points - points_used}</p>
+</div>
+
+<div>
+    <h2>Attributes</h2>
+    
+</div>
